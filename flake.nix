3dffc0bc3610b0fc9443176c643da15947dbb970
heyzec <61238538+heyzec@users.noreply.github.com>
2024-09-12 19:25:15 +0800

    feat: init repo

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Template for a direnv shell, with NodeJS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs_20
+        yarn
+      ];
+    };
+  };
+}
