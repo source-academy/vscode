e82a82765077bd2ff68bbe9327523dfc34b6cc98
heyzec <61238538+heyzec@users.noreply.github.com>
2024-10-02 12:57:17 +0800

    add webview, icon

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { NodeDependenciesProvider, setupTreeView } from "./view/test";
 import { runStepper } from "./commands/stepper";
 import { runLanguagePicker } from "./commands/language";
 import { setupStatusBar } from "./statusbar/status";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  setupTreeView();
+
   context.subscriptions.push(
     vscode.commands.registerCommand("sa-vscode.pick", () =>
       runLanguagePicker(context),
