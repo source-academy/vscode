966e38fb606998634561e6ca405081639ab7d998
heyzec <61238538+heyzec@users.noreply.github.com>
2025-03-12 15:56:13 +0800

    chore: remove unused deps after new approach

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -1,4 +1,26 @@
-import { Chapter } from "js-slang/dist/types";
+// import { Chapter } from "js-slang/dist/types";
+enum Chapter {
+  SOURCE_1 = 1,
+  SOURCE_2 = 2,
+  SOURCE_3 = 3,
+  SOURCE_4 = 4,
+  FULL_JS = -1,
+  HTML = -2,
+  FULL_TS = -3,
+  PYTHON_1 = -4,
+  PYTHON_2 = -5,
+  PYTHON_3 = -6,
+  PYTHON_4 = -7,
+  FULL_PYTHON = -8,
+  SCHEME_1 = -9,
+  SCHEME_2 = -10,
+  SCHEME_3 = -11,
+  SCHEME_4 = -12,
+  FULL_SCHEME = -13,
+  FULL_C = -14,
+  FULL_JAVA = -15,
+  LIBRARY_PARSER = 100,
+}
 
 const SECTION = "\u00A7";
 
