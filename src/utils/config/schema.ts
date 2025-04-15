aae92a9a57d888d6599c1208e58b71278e3bc41c
heyzec <61238538+heyzec@users.noreply.github.com>
2025-04-15 17:02:46 +0800

    fix: typo in extension setting id

--- src/utils/config/schema.ts
+++ src/utils/config/schema.ts
@@ -3,7 +3,7 @@ import { ConfigSchemaUnion } from "./types";
 export default {
   frontendBaseUrl: {
     type: "string",
-    default: "http://localhost:8000",
+    default: "https://frontend.cloud.heyzec.dedyn.io",
   },
   workspaceFolder: { type: "string", default: "" },
 } satisfies Record<string, ConfigSchemaUnion>;
